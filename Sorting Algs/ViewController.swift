d2649b2229120de7242f52c3e147cbe3de51e0f8
amoham37 <amoham811@gmail.com>
2019-08-06 13:43:28 -0500

    Initial Commit

---
+++ Sorting Algs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sorting Algs
+//
+//  Created by Ahmet Mohammed on 8/6/19.
+//  Copyright © 2019 Ahmet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Sorting AlgsTests/Sorting_AlgsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Sorting_AlgsTests.swift
+//  Sorting AlgsTests
+//
+//  Created by Ahmet Mohammed on 8/6/19.
+//  Copyright © 2019 Ahmet. All rights reserved.
+//
+
+import XCTest
+@testable import Sorting_Algs
+
+class Sorting_AlgsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
